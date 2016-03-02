c0fcc46c0645c413a1831bb7282de0bb09b369a9
Simon Yousoufov <syousoufov@fuzzproductions.com>
2016-03-02 11:34:04 -0500

    Move relation checker into a trait:

--- src/EloquentRepository.php
+++ src/EloquentRepository.php
@@ -2,6 +2,7 @@
 
 namespace Fuzz\MagicBox;
 
+use Fuzz\MagicBox\Utility\ChecksRelations;
 use Illuminate\Database\Eloquent\Model;
 use Fuzz\MagicBox\Contracts\Repository;
 use Illuminate\Database\Eloquent\Builder;
@@ -16,6 +17,8 @@ use Illuminate\Support\Str;
 
 class EloquentRepository implements Repository
 {
+	use ChecksRelations;
+
 	/**
 	 * An instance variable specifying the model handled by this repository.
 	 *
@@ -735,54 +738,6 @@ class EloquentRepository implements Repository
 		$input = $this->getInput();
 
 		return $input[self::KEY_NAME];
-	}
-
-	/**
-	 * Safely determine if the specified key name is a relation on the model instance
-	 *
-	 * @todo use PHP7 return type reflection solution as well
-	 *
-	 * @param \Illuminate\Database\Eloquent\Model $instance
-	 * @param string                              $key
-	 * @param string                              $model_class
-	 * @return \Illuminate\Database\Eloquent\Relations\Relation|bool
-	 */
-	protected function isRelation(Model $instance, $key, $model_class)
-	{
-		// Not a relation method
-		if (! method_exists($instance, $key)) {
-			return false;
-		}
-
-		$relation      = null;
-		$safe_instance = new $model_class;
-
-		// Get method, dirty and imperfect
-		$reflected_method = (new \ReflectionMethod($safe_instance, $key))->__toString();
-
-		$supported_relations = [
-			BelongsTo::class,
-			HasOne::class,
-			HasMany::class,
-			BelongsToMany::class,
-		];
-
-		// Find which, if any, of the supported relations are present in the reflected string
-		foreach ($supported_relations as $supported_relation) {
-			if (strpos($reflected_method, $supported_relation) !== false) {
-				$relation = $instance->$key();
-				break;
-			}
-		}
-
-		// If the ReflectionMethod guess fails, try to guess based on the concrete return type of a safe instance
-		// of the model
-		if (is_null($relation) && ($safe_instance->$key() instanceof Relation)) {
-			// If the method returns a Relation, we can safely call it
-			$relation = $instance->$key();
-		}
-
-		return is_null($relation) ? false : $relation;
 	}
 
 	/**
---
+++ src/Utility/ChecksRelations.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Fuzz\MagicBox\Utility;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+trait ChecksRelations
+{
+	/**
+	 * Safely determine if the specified key name is a relation on the model instance
+	 *
+	 * @todo use PHP7 return type reflection solution as well
+	 *
+	 * @param \Illuminate\Database\Eloquent\Model $instance
+	 * @param string                              $key
+	 * @param string                              $model_class
+	 * @return \Illuminate\Database\Eloquent\Relations\Relation|bool
+	 */
+	protected function isRelation(Model $instance, $key, $model_class)
+	{
+		// Not a relation method
+		if (! method_exists($instance, $key)) {
+			return false;
+		}
+
+		$relation      = null;
+		$safe_instance = new $model_class;
+
+		// Get method, dirty and imperfect
+		$reflected_method = (new \ReflectionMethod($safe_instance, $key))->__toString();
+
+		$supported_relations = [
+			BelongsTo::class,
+			HasOne::class,
+			HasMany::class,
+			BelongsToMany::class,
+		];
+
+		// Find which, if any, of the supported relations are present in the reflected string
+		foreach ($supported_relations as $supported_relation) {
+			if (strpos($reflected_method, $supported_relation) !== false) {
+				$relation = $instance->$key();
+				break;
+			}
+		}
+
+		// If the ReflectionMethod guess fails, try to guess based on the concrete return type of a safe instance
+		// of the model
+		if (is_null($relation) && ($safe_instance->$key() instanceof Relation)) {
+			// If the method returns a Relation, we can safely call it
+			$relation = $instance->$key();
+		}
+
+		return is_null($relation) ? false : $relation;
+	}
+}
