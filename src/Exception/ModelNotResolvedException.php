857a87a733866e7feea48df1a663e3f5d9565469
Simon Yousoufov <syousoufov@fuzzproductions.com>
2017-03-06 16:41:54 -0500

    Fix typo

--- src/Exception/ModelNotResolvedException.php
+++ src/Exception/ModelNotResolvedException.php
@@ -20,7 +20,7 @@ class ModelNotResolvedException extends RuntimeException
 	 * @param int             $code
 	 * @param \Exception|null $previous
 	 */
-	public function __construct($message = 'Model could not be resolbed.', $code = 0, \Exception $previous = null)
+	public function __construct($message = 'Model could not be resolved.', $code = 0, \Exception $previous = null)
 	{
 		parent::__construct($message, $code, $previous);
 	}
